a26c8fa9f3791434d22ec8c16db8d930cf117af2
MRPrio <maksudurprio.mp@gmail.com>
2022-12-28 15:31:20 +0600

    all routes added on nav bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { router } from "./Routes/Route";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-screen-lg mx-auto">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -1,4 +1,7 @@
+import About from "../Pages/About/About";
 import Home from "../Pages/Home/Home";
+import Media from "../Pages/Media/Media";
+import Message from "../Pages/Message/Message";
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../Outlet/Main");
@@ -11,6 +14,18 @@ export const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/media',
+                element:<Media></Media>
+            },
+            {
+                path:'/message',
+                element:<Message></Message>
+            },
+            {
+                path:'/about',
+                element:<About></About>
             }
         ]
     }
