68bc2d4534f407df45a337e5d1d8a8678f0ab56d
MRPrio <maksudurprio.mp@gmail.com>
2022-12-29 08:31:16 +0600

    trending section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './Form/Form';
+import Trending from './Trending/Trending';
 
 const Home = () => {
     return (
         <div>
             <Form></Form>
+            <Trending></Trending>
         </div>
     );
 };
